13a2947d9634af9ad6a2cdbfd5db2976a626d4bd
Rhythm Gulia <rhythmgulia28@gmail.com>
2025-12-04 22:28:00 +0530

    Update auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -14,8 +14,8 @@ const signToken = (id) =>
 const sendTokenCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 };
